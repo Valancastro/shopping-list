2c8d051f642438968f5b1926b964a711831e83de
unknown <castrovictor712@gmail.com>
2015-10-30 17:46:27 -0700

    cleaned code

--- js/app.js
+++ js/app.js
@@ -1,55 +1,58 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
+  var total = 0
 
-    var total = 0
 
-         $('form').submit(function(e){
-            
-            
+  $('form').submit(function(e) {
 
-             var item = $('#add-items').val()
-             var price = $('#add-price').val()
+    var item = $('#add-items').val()
+    var price = $('#add-price').val()
 
-                if (item =='' ) {
+    if (item == '') {
 
-                   // alert('Must enter a Item!')
+      // alert('Must enter a Item!')
 
-                } else if (parseFloat(price) < 0) {
+    } else if (parseFloat(price) < 0) {
 
-                     // alert('price cannot be less than Zero')
-                    }
-                     else { 
+      // alert('price cannot be less than Zero')
+    } else {
 
-                        total += parseFloat(price);  
+      total += parseFloat(price);
 
-                        $('.total-subtotal').text("" + total)    
+      $('.total-subtotal').text("" + total)
 
-                        $('.list-area').prepend(' <div class="no-strikethrough"><i class="fa fa-bomb" style="float: left; cursor:pointer;"></i><span class="item-list">' + item + '</span><div class="price"><span class="price-list">'+ price +'</span></div><i class="fa fa-check-circle" class="check" style="cursor:pointer;"></i></div>')
-          
-                        this.reset()
-                    }
+      $('.list-area').prepend(' <div class="no-strikethrough"><i class="fa fa-bomb bomb" style="float: left; cursor:pointer;"></i><span class="item-list">' + item + '</span><div class="price"><span class="price-list">' + price + '</span></div><i class="fa fa-check-circle check" class="check" style="cursor:pointer;"></i></div>')
 
-                        e.preventDefault();
+      $('.bomb').on("click", function(event) {
+        event.preventDefault();
+        $(this).parent().fadeOut("slow")
+      })
 
-    });
+      $('.check').parent().on("click",function(e) {
+           e.preventDefault();
+         $(this).toggleClass('strikethrough');
+         
 
-        $('.reset').click(function(e){
-             $('.list-area').empty();
-             $('.total-subtotal').text('');
-             total = 0;
+         });
 
-             
+      this.reset()
 
-        });
+    }
+
+    e.preventDefault();
+
+  });
+
+
+  $('.reset').click(function(e) {
+    $('.list-area').empty();
+    $('.total-subtotal').text('');
+    total = 0;
+
+
+
+  });
 
 
 
 });
-
-
-
-
-
-
-
- 
