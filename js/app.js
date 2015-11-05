5e5b1fde9394af777d053dd9720dff5e7050a3d5
unknown <castrovictor712@gmail.com>
2015-11-04 20:25:35 -0800

    fixed item delete and check issue

--- js/app.js
+++ js/app.js
@@ -5,58 +5,56 @@ $(document).ready(function() {
 
   $('form').submit(function(e) {
 
-      var item = $('#add-items').val()
-      var price = $('#add-price').val()
+    var item = $('#add-items').val()
+    var price = $('#add-price').val()
 
-      if (item == '') {
+    if (item == '') {
 
-        alert('Must enter a Item!')
+      alert('Must enter a Item!')
 
-      } else if (parseFloat(price) < 0) {
+    } else if (parseFloat(price) < 0) {
 
-         alert('price cannot be less than Zero')
-      } else {
+      alert('price cannot be less than Zero')
+    } else {
 
-        total += parseFloat(price);
+      total += parseFloat(price);
 
-        $('.total-subtotal').text("" + total)
+      $('.total-subtotal').text("" + total)
 
-        $('.list-area').prepend(' <div class="no-strikethrough"><i class="fa fa-bomb bomb" data-price="'+ price +'" style="float: left; cursor:pointer;"></i><span class="item-list">' + item + '</span><div class="price"><span class="price-list">' + price + '</span></div><a class="check-circle"><i class="fa fa-check-circle circle" style="cursor:pointer;"></i></a></div>')
+      $('.list-area').prepend(' <div class="no-strikethrough"><i class="fa fa-bomb bomb" data-price="' + price + '" style="float: left; cursor:pointer;"></i><span class="item-list">' + item + '</span><div class="price"><span class="price-list">' + price + '</span></div><a class="check-circle"><i class="fa fa-check-circle circle" style="cursor:pointer;"></i></a></div>')
 
-        $('.bomb').on("click", function(event) {
-          event.preventDefault();
-          $(this).parent().remove();
-           var priceRemove = $(this).data("price")
-           total -= parseFloat(priceRemove) ;      
-          $('.total-subtotal').text("" + total)
+      this.reset()
+    }
 
+    $('#add-items').focus();
 
-        })
+    e.preventDefault();
 
-        $('.circle').click(function(e) {
-          e.preventDefault();
-          $(this).toggleClass('fa-circle-o fa-circle-check');
-          $(this).parent().parent().toggleClass('strikethrough no-strikethrough')
-          $(this).unbind(alert);
+  });
 
-        })
-        this.reset()
+  $('.list-area').on('click', 'i.bomb', function(event) {
+   event.preventDefault();
+   $(this).parent().remove();
+   var priceRemove = $(this).data("price")
+   total -= parseFloat(priceRemove);
+   $('.total-subtotal').text("" + total)
+  });
 
-  }
-
-  e.preventDefault();
-
-});
+   $('.list-area').on('click', 'i.circle', function(event) {
+     event.preventDefault();
+      $(this).toggleClass('fa-circle-o fa-circle-check');
+      $(this).parent().parent().toggleClass('strikethrough no-strikethrough')
+    });
 
 
-$('button').click(function(e) {
-$('.list-area').empty();
-$('.total-subtotal').text('');
-total = 0;
+  $('button').click(function(e) {
+    $('.list-area').empty();
+    $('.total-subtotal').text('');
+    total = 0;
 
 
 
-});
+  });
 
 
 
